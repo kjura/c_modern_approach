6756c8cb248fc566e5c4a069b2885a6c5ef73828
kjura <bulsit565@gmail.com>
2021-11-14 17:13:32 +0100

    Upload completed Project1 form Chapter 3.

--- Chapter3/chapter3project1.c
+++ Chapter3/chapter3project1.c
@@ -5,10 +5,15 @@ int main(void)
   
   int month, day, year;
 
+  // Conversion specification for printf() function: %m.pX or %-m.pX
+  // m and p are integer constants, X is a letter.
+  // m - minimal field width , along with "-" can cause left justification
+  // ########################################
+  // p - precision, X - conversion specifier
 
   printf("Enter a date (mm/dd/yyyy): ");
   scanf("%d/%d/%d", &month, &day, &year);
-  printf("You entered the date %d%d%d\n", year, month, day);
+  printf("You entered the date %d%.2d%.2d\n", year, month, day);
 
   return 0;
 }
