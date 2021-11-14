2cb0691620c879f0f83b918c4365728dfca48559
kjura <kjura@users.noreply.github.com>
2021-11-14 11:01:48 +0100

    Add files via upload

---
+++ Chapter2/Project3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286f
+
+int main(void)
+{
+    float r;
+    printf("Please provide the radius of a sphere ");
+    scanf("%f", &r);
+    float v = 4.0f / 3.0f * PI * r * r * r;
+
+    printf("Volume of a sphere with a 10-meter radius is %f\n", v);
+    
+
+
+    return 0;
+}
