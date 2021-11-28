988a7b384a3503413f2d7b7cd15c135ac9703806
kjura <kjura@users.noreply.github.com>
2021-11-28 15:39:47 +0100

    Create exercise2.c

---
+++ Chapter7/Exercises/exercise2.c
@@ -0,0 +1,19 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+
+
+// Chapter7 Exercise2
+
+/*
+
+(a) 010E2 -> legal constant (octal number (decimal 8) with an exponent) -> Type: floating-point
+
+(b) 32.1E+5 -> legal constant -> Type: floating-point
+
+(c) 0790 -> not legal -> 9 is not possible in the octal number
+
+(d) 100_000 -> not legal -> underscore between
+
+(e) 3.978e-2 -> legal constant -> floating-point
+
+*/
